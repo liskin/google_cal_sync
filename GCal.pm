fc20b38e0520f2e80545a2ce3b5f3aa6ca9b883e
Tomas Janousek <tomi@nomi.cz>
2011-11-13 23:35:10 +0100

    initial commit

---
+++ GCal.pm
@@ -0,0 +1,94 @@
+# Copyright (C) 2011, Tomas Janousek. BSD license.
+# vim:set et:
+package GCal;
+
+use strict;
+use warnings;
+
+use Net::Google::Calendar;
+use Data::Dumper;
+
+# Constructor, to be called as:
+# my $gcal = GCal->new( user => '...', pass => '... );
+sub new {
+    my ($class, %param) = @_;
+    my $self = { cal => Net::Google::Calendar->new, %param };
+    $self->{cal}->login( $self->{user}, $self->{pass} ) or die;
+    bless $self, $class;
+    return $self;
+}
+
+# Set current calendar by its name.
+# $cal->set_calendar( 'Facebook events' )
+sub set_calendar {
+    my ($self, $cal) = @_;
+    $self->{cal}->set_calendar( grep { $_->title eq $cal }
+        $self->{cal}->get_calendars( 1 ) ) or die;
+}
+
+# Fill in Net::Google::Calendar::Entry from a simple hash:
+# { title    => 'event title'
+# , content  => 'event description'
+# , location => 'event location'
+# , url      => 'event url'
+# , when     => [ <DateTime> of start, <DateTime> of end ]
+# , id       => 'unique event id'
+# }
+sub x2entry {
+    my ($ev, $entry) = @_;
+
+    $entry->title(    $ev->{title}    );
+    $entry->content(  $ev->{url} . "\n\n" . $ev->{content}  );
+    $entry->location( $ev->{location} );
+    $entry->when(  @{ $ev->{when} }   );
+    $entry->extended_property( 'id',      $ev->{id}      );
+
+    return;
+}
+
+# Take a list of entries in the format expected by x2entry and put them online.
+# New events are added, old are updated (unconditionally, some local
+# hashing/caching should be implemented, TODO), those not present are deleted.
+sub update_entries {
+    my $self = shift;
+    my $entries;
+
+    for my $entry ( $self->{cal}->get_events ) {
+        my $id = $entry->extended_property->{id};
+        if ( not $id ) {
+            warn "entry without id";
+            next;
+        }
+        $entries->{ $id } = $entry;
+    }
+
+    my $ev;
+    while ( $ev = shift ) {
+        my $new = 0;
+        my $entry = $entries->{ $ev->{id} };
+        unless ( $entry ) {
+            $entry = Net::Google::Calendar::Entry->new;
+            $new   = 1;
+        }
+
+        x2entry( $ev, $entry );
+
+        if ( $new ) {
+            $self->{cal}->add_entry( $entry );
+            print "adding " . $entry->extended_property->{id} . "\n";
+        } else {
+            $self->{cal}->update_entry( $entry );
+            delete $entries->{ $ev->{id} };
+            print "updating " . $entry->extended_property->{id} . "\n";
+        }
+    }
+
+    for my $entry ( values %$entries ) {
+        $self->{cal}->delete_entry( $entry );
+        print "deleting " . $entry->extended_property->{id} . "\n";
+    }
+
+    return;
+}
+
+1;
---
+++ ICal.pm
@@ -0,0 +1,62 @@
+# Copyright (C) 2011, Tomas Janousek. BSD license.
+# vim:set et:
+package ICal;
+
+use strict;
+use warnings;
+
+use iCal::Parser;
+
+# Load all events from an iCal data in a string into a simple structure as
+# expected by my GCal module.
+sub load_ical {
+    my $data = shift;
+    my $parser = iCal::Parser->new;
+    my $hash = $parser->parse_strings( $data );
+    return uniq( map { ical2x( $_ ) }
+                 map { values %$_ } map { values %$_ }
+                 map { values %$_ } map { values %$_ }
+                 $hash->{events} );
+}
+
+# Convert to the simple structure used in GCal.
+sub ical2x {
+    my $ev = shift;
+    return {
+        title    => ical_unescape( "$ev->{SUMMARY}"     ),
+        content  => ical_unescape( "$ev->{DESCRIPTION}" ),
+        location => ical_unescape( "$ev->{LOCATION}"    ),
+        url      => ical_unescape( "$ev->{URL}"         ),
+        #partstat => "$ev->{PARTSTAT}",
+        id       => "$ev->{UID}",
+        #lastmod  => $ev->{"LAST-MODIFIED"}->strftime( "%s" ),
+        when     => [ $ev->{DTSTART}, $ev->{DTEND} ],
+    };
+}
+
+sub ical_unescape {
+    my $_ = shift;
+    s/\\([;,])/$1/g;
+    s/\\n/\n/ig;
+    s/\\\\/\\/g;
+    return $_;
+}
+
+# Bloody iCal::Parser splits events, so I join them again. So stupid.
+sub uniq {
+    my $evs;
+
+    while ( my $ev = shift ) {
+        push @{ $evs->{ $ev->{id} } }, $ev;
+    }
+
+    for my $ev ( values %$evs ) {
+        @$ev = sort { DateTime->compare( $a->{when}->[0], $b->{when}->[0] ) } @$ev;
+        $ev->[0]->{when} = [ $ev->[0]->{when}->[0], $ev->[ $#$ev ]->{when}->[1] ];
+        @$ev = ( $ev->[0] );
+    }
+
+    return map { @$_ } values %$evs;
+}
+
+1;
