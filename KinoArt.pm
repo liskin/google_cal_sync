05d6f8b5565c011a3347aa1d9d1c9a06ac84746c
Tomas Janousek <tomi@nomi.cz>
2011-11-14 01:02:22 +0100

    Kino Art, configuration file

---
+++ KinoArt.pm
@@ -0,0 +1,55 @@
+# Copyright (C) 2011, Tomas Janousek. BSD license.
+# vim:set et:
+package KinoArt;
+
+use strict;
+use warnings;
+use utf8;
+
+use LWP::UserAgent;
+use DateTime;
+use Digest::SHA1 'sha1_hex';
+use Encode;
+
+
+sub download {
+    my $ua = LWP::UserAgent->new;
+    return parse( $ua->get( 'http://www.kinoartbrno.cz/' )->decoded_content );
+}
+
+sub parse {
+    my $page = shift;
+    my (@dates, @events);
+
+    while ( $page =~ m|<h2 class="barva_mesice">.. \&nbsp;\&nbsp;(\d+)\. (\d+). (\d+)</h2>|gs ) {
+	push @dates, [ pos $page, $1, $2, $3 ];
+    }
+
+    while ( my $date = shift @dates ) {
+	pos $page = $date->[0];
+	while ( $page =~ m|<td class="casVS".*?(\d+)\.(\d+).*?href="(\S+?)">(.*?)</a>|gs ) {
+	    last if @dates and pos $page > $dates[0]->[0];
+	    my $start = dt( @$date[1..3], $1, $2 );
+	    my $end   = $start + DateTime::Duration->new( hours => 2 );
+	    my $event = { title    => $4
+			, content  => ''
+			, location => 'Kino Art, Cihlářská 19'
+			, url      => $3
+			, when     => [ $start, $end ]
+			, id       => sha1_hex( "$3 $start" )
+			};
+	    next if $event->{title} =~ /kino nehraje/;
+	    push @events, $event;
+	}
+    }
+
+    return @events;
+}
+
+sub dt {
+    my %param;
+    @param{qw/day month year hour minute/} = @_;
+    return DateTime->new( %param );
+}
+
+1;
---
+++ sync.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+# vim:set et:
+
+use strict;
+use warnings;
+
+use LWP::UserAgent;
+use ICal;
+use GCal;
+use KinoArt;
+
+my $conf = eval `cat private.conf`;
+
+my $ua = LWP::UserAgent->new;
+my $gcal = GCal->new( user => $conf->{user}, pass => $conf->{pass} );
+
+$gcal->set_calendar( 'Facebook events' );
+$gcal->update_entries( ICal::load_ical( $ua->get( $conf->{fb_url} )->decoded_content ) );
+
+$gcal->set_calendar( 'Kino Art' );
+$gcal->update_entries( KinoArt::download );
