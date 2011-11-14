58637fab199dea24750fa542ba18bee8b9adde28
Tomas Janousek <tomi@nomi.cz>
2011-11-15 00:42:35 +0100

    and fancy facebook icons as well

--- sync.pl
+++ sync.pl
@@ -15,7 +15,9 @@ my $ua = LWP::UserAgent->new;
 my $gcal = GCal->new( user => $conf->{user}, pass => $conf->{pass} );
 
 $gcal->set_calendar( 'Facebook events' );
-$gcal->update_entries( ICal::load_ical( $ua->get( $conf->{fb_url} )->decoded_content ) );
+$gcal->update_entries(
+    map { $_->{icon} = 'http://facebook.com/favicon.ico'; $_ }
+        ICal::load_ical( $ua->get( $conf->{fb_url} )->decoded_content ) );
 
 $gcal->set_calendar( 'Kino Art' );
 $gcal->update_entries( KinoArt::download );
