8727cc589b3bad78809069933834a8c5b7ad6259
Tomas Janousek <tomi@nomi.cz>
2012-04-14 13:01:36 +0200

    Allow specifying things to sync as command line arguments.

--- sync.pl
+++ sync.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 # vim:set et:
 
+use 5.010;
 use strict;
 use warnings;
 
@@ -13,15 +14,26 @@ my $conf = eval `cat private.conf`;
 my $ua = LWP::UserAgent->new;
 my $gcal = GCal->new( user => $conf->{user}, pass => $conf->{pass} );
 
-$gcal->set_calendar( 'Facebook events' );
-$gcal->update_entries(
-    map { $_->{icon} = 'http://facebook.com/favicon.ico'; $_ }
-        ICal::load_ical( $ua->get( $conf->{fb_url} )->decoded_content ) );
+my $do = {
+    facebook => sub {
+        $gcal->set_calendar( 'Facebook events' );
+        $gcal->update_entries(
+            map { $_->{icon} = 'http://facebook.com/favicon.ico'; $_ }
+                ICal::load_ical( $ua->get( $conf->{fb_url} )->decoded_content ) );
+    },
 
-use KinoArt;
-$gcal->set_calendar( 'Kino Art' );
-$gcal->update_entries( KinoArt::download );
+    kinoart => sub {
+        use KinoArt;
+        $gcal->set_calendar( 'Kino Art' );
+        $gcal->update_entries( KinoArt::download );
+    },
 
-use Alterna;
-$gcal->set_calendar( 'Alterna' );
-$gcal->update_entries( Alterna::download );
+    alterna => sub {
+        use Alterna;
+        $gcal->set_calendar( 'Alterna' );
+        $gcal->update_entries( Alterna::download );
+    },
+};
+
+my @todo = @ARGV ? @ARGV : keys %$do;
+$_->() for @$do{ @todo };
